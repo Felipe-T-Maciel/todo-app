67352423ee2e952f6b64a332b60f267fff4743a8
FELIPE TOMIO MACIEL <felipe_t_maciel@estudante.sesisenai.org.br>
2023-05-04 14:51:22 -0300

    terceiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { stringify } from '@angular/core/src/util';
+import { parse } from 'querystring';
 
 interface Tarefa{
   nome: string
   desc: string
+  categoria: string
 }
 
-interface Categoria{
-  categoria2: string
-}
 
 @Component({
   selector: 'app-root',
@@ -15,30 +15,40 @@ interface Categoria{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  ngOnInit():void{
+    this.atts = JSON.parse(window.localStorage.getItem("Lista de tarefas"))
+  }
+
   title = 'todo-app';
   nome: string = '';
   desc: string = '';
   categoria: string = '';
   atts: Tarefa[] = []
+  indices: 0
 
   Tarefa={
     nome: '',
-    desc: ''
+    desc: '',
+    categoria: ''
   }
 
   novaTarefa():void{
     const att:Tarefa ={
       nome: this.Tarefa.nome,
-      desc: this.Tarefa.desc
+      desc: this.Tarefa.desc,
+      categoria: this.Tarefa.categoria
     }
+
     this.atts.push(att)
-    this.Tarefa.nome = ''
-    this.Tarefa.desc = ''
+    this.Tarefa.nome = null
+    this.Tarefa.desc = null
+    this.Tarefa.categoria = null
     localStorage.setItem("Lista de tarefas", JSON.stringify(this.atts))
   }
 
   Apaga(indice){
-    console.log(indice);
+    this.indices
     this.atts.splice(indice, 1)
     localStorage.removeItem("att")
     localStorage.setItem("Lista de tarefas", JSON.stringify(this.atts))
